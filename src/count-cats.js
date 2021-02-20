640461a18448326fa83b24467f805b249fad8fbe
github-AKZhuk <lesha.zhuk@gmail.com>
2021-02-20 23:25:09 +0300

    feat: implement dream-team task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(matrix) {
+  let result = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let n = 0; n < matrix[i].length; n++) {
+      matrix[i][n] == "^^" ? (result = result + 1) : result;
+    }
+  }
+  return result;
   // remove line with error and write your code here
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let nameDreamTeam = "";
+  if (Array.isArray(members) == true) {
+    members = members.map((name) =>
+      typeof name === "string" ? name.trim() : ""
+    );
+    members.sort().forEach((name) => {
+      if (typeof name === "string") {
+        nameDreamTeam = nameDreamTeam + name.substr(0, 1).toUpperCase();
+      }
+    });
+  } else {
+    nameDreamTeam = false;
+  }
+  return nameDreamTeam;
 };
